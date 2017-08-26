74e081deac771ccf42cd1215e46eab4c2bd6c99c
BboyStatix <statix231@hotmail.com>
2017-08-26 22:17:39 +0800

    concurrency scripts, update server.js syntax and port

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3001, () => {
+  console.log('Example app listening on port 3001!')
 })
