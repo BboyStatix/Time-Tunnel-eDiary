7bc5394d9aeed9fcc22cd789f849dc4b7874539b
BboyStatix <statix231@hotmail.com>
2017-08-27 15:28:32 +0800

    set up proxy server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+// const path = require('path');
+
+// app.use(express.static(path.join(__dirname, 'client/build')))
+
+// app.get('/hello', (req,res) => {
+//   console.log('hello')
+// })
 
 app.listen(3001, () => {
   console.log('Example app listening on port 3001!')
