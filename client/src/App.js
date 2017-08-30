3745f409053732bafed3fad3969eb1c422f7abdf
BboyStatix <statix231@hotmail.com>
2017-08-30 15:13:01 +0800

    Header.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react'
 import { Link, Switch, Route } from 'react-router-dom'
 
-import logo from './logo.svg'
 import './App.css'
+
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <Header />
         <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
         <Switch>
           <Route exact path='/' render={() => <Link to='/temp'>Click Here</Link>}/>
           <Route exact path='/temp' render={() => <Link to='/'>Go Back</Link>}/>
         </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-
 //for use in production. serve the build folder for files optimized
 //for production created by npm run build in client directory
-// app.use(express.static(path.join(__dirname, 'client/build')))
+//const path = require('path')
+//app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.listen(3001, () => {
   console.log('Example app listening on port 3001!')
