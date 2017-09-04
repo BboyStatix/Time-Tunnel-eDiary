a1674a1aa0bdf73c906f4bb0aa0eb2a5b3bbd4fa
BboyStatix <statix231@hotmail.com>
2017-09-04 21:16:56 +0800

    npm install mongodb

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express()
 //const path = require('path')
 //app.use(express.static(path.join(__dirname, 'client/build')))
 
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/animals', function (err, db) {
+  if (err) throw err
+
+  db.collection('mammals').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
+
 app.listen(3001, () => {
   console.log('Example app listening on port 3001!')
 })
