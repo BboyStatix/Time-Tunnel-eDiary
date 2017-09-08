218d6185fd53989a26cae1ee02e08b2e9d1bbf90
BboyStatix <statix231@hotmail.com>
2017-09-08 23:34:20 +0800

    UserSchema and database connection with mongoose

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 //for use in production. serve the build folder for files optimized
 //for production created by npm run build in client directory
 //const path = require('path')
 //app.use(express.static(path.join(__dirname, 'client/build')))
 
-const mongoose = require('mongoose')
-
+//connect to database
 mongoose.connect('mongodb://localhost/test')
-
-var db = mongoose.connection
+const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
-  // we're connected!
-  console.log("We're connected")
+  console.log("Connected to database")
 })
 
 app.listen(3001, () => {
-  console.log('Example app listening on port 3001!')
+  console.log('App listening on port 3001!')
 })
