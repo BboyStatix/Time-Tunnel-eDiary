e6b1d565038fa1cc04e7f9883ab7240060a56be3
BboyStatix <statix231@hotmail.com>
2018-02-09 19:28:12 +0800

    smaller header, css adjustments

--- client/src/Header.js
+++ client/src/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Time Tunnel Website</h2>
+        <h4>Time Tunnel Website</h4>
       </div>
     )
   }
