ccca001fd7765a0208612dd7cdf9c90345e77fbc
BboyStatix <statix231@hotmail.com>
2018-04-10 22:49:56 +0800

    AudioModal, audiocontainer play button song,artist,album,info columns

--- client/src/AudioContainer.js
+++ client/src/AudioContainer.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
-import ReactAudioPlayer from 'react-audioplayer'
 import expandLogo from './img/expand.svg'
 
 import { Link } from 'react-router-dom'
+import AudioModal from './AudioModal'
 
 class AudioContainer extends Component {
   constructor(props){
     super(props)
     this.fetchEntries = this.fetchEntries.bind(this)
+    this.showModal = this.showModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
     this.expandContainer = this.expandContainer.bind(this)
     this.closeContainerModal = this.closeContainerModal.bind(this)
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
     this.handleSearch = this.handleSearch.bind(this)
     this.state = {
       entries: [],
+      modalVisible: false,
+      songName: '',
+      filename: '',
       expandedEntries: [],
       containerExpanded: false
     }
@@ -39,6 +44,19 @@ class AudioContainer extends Component {
     .then((json) => {
       this.setState({entries: json.entries, expandedEntries: json.entries})
     })
+  }
+
+  showModal(songName, filename) {
+    this.setState({modalVisible: true, songName: songName, filename:  filename})
+  }
+
+  closeModal(e) {
+    const modal = document.getElementById('myModal')
+    const cross = document.getElementById('cross')
+    const closeButton = document.getElementById('closeButton')
+    if(e.target === modal || e.target === cross || e.target === closeButton){
+      this.setState({modalVisible: false})
+    }
   }
 
   expandContainer() {
@@ -103,6 +121,14 @@ class AudioContainer extends Component {
     return (
       <div>
         {
+          this.state.modalVisible ?
+          <div onClick={this.closeModal}>
+            <AudioModal songName={this.state.songName} filename={this.state.filename}/>
+          </div>
+          :
+          null
+        }
+        {
           this.state.containerExpanded ?
           <div className="custom-modal" id="containerModal" onClick={this.closeContainerModal}>
             <div className="modal-dialog" role="document">
@@ -143,19 +169,31 @@ class AudioContainer extends Component {
             Audio
             <img className='expandLogo float-right' src={expandLogo} onClick={this.expandContainer}/>
           </div>
-          <div className="card-body">
-            <div className="container">
-              {
-                this.state.entries.map((entry,idx) =>
-                  <div className="row" key={idx}>
-                    <ReactAudioPlayer
-                      playlist={[{name: entry.name, src: "/audio/view?jwt=" + localStorage.jwt + "&filename=" + entry.filename}]}
-                      style={{width: '100%'}}
-                    />
-                  </div>
-                )
-              }
-            </div>
+          <div className="card-body" style={{padding: 0}}>
+            <table className="table table-hover main-page-table">
+              <thead>
+                <tr>
+                  <th scope="col">Song</th>
+                  <th scope="col">Artist</th>
+                  <th scope="col">Album</th>
+                  <th scope="col">Information</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.entries.map((entry,idx) =>
+                    <tr key={idx}>
+                      <td className="text-truncate">{entry.name}</td>
+                      <td className="diary-description text-truncate">{entry.artist}</td>
+                      <td>{entry.album}</td>
+                      <td>{entry.information}</td>
+                      <td><button className="btn btn-outline-danger" onClick={() => this.showModal(entry.name, entry.filename)}>Play</button></td>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
---
+++ client/src/AudioModal.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import ReactAudioPlayer from 'react-audioplayer'
+
+class AudioModal extends Component {
+  render() {
+    return (
+      <div className="custom-modal" id="myModal">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{this.props.songName}</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span id="cross" aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            {/* <div className="modal-body diary-body">
+              <ReactAudioPlayer
+                playlist={[{name: this.props.name, src: "/audio/view?jwt=" + localStorage.jwt + "&filename=" + this.props.filename}]}
+                style={{width: '100%'}}
+              />
+            </div> */}
+            <div className="modal-footer">
+              <button id="closeButton" type="button" className="btn btn-secondary">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AudioModal
